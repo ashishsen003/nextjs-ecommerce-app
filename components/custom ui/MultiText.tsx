a295fcf1d92d3db6680ee9e0625a3d765b2597d6
ashishsen003 <ashishsen997@gmail.com>
2025-07-30 23:29:22 +0530

    added collcection, tags fileds in frontend with api's

---
+++ components/custom ui/MultiText.tsx
@@ -0,0 +1,58 @@
+"use client";
+import { useState } from "react";
+import { Input } from "../ui/input";
+import { Badge } from "../ui/badge";
+import { Button } from "../ui/button";
+import { X } from "lucide-react";
+
+interface MultiTextProps {
+  placeholder: string;
+  value: string[];
+  onChange: (value: string) => void;
+  onRemove: (value: string) => void;
+}
+
+const MultiText: React.FC<MultiTextProps> = ({
+  placeholder,
+  value,
+  onChange,
+  onRemove,
+}) => {
+  const [inputValue, setInputValue] = useState("");
+
+  const addTag = (tag: string) => {
+    if (tag.trim() !== "") {
+      onChange(tag);
+      setInputValue("");
+    }
+  };
+
+  return (
+    <>
+      <Input
+        placeholder={placeholder}
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        onKeyDown={(e) => {
+          if (e.key === "Enter" && inputValue.trim() !== "") {
+            e.preventDefault();
+            addTag(inputValue);
+          }
+        }}
+      />
+
+      <div className="flex flex-wrap gap-1 mt-4">
+        {value.map((tag, index) => (
+          <Badge key={index} className="bg-grey-1 text-white rounded-full">
+            {tag}
+            <Button className="ml-2 rounded-full outline-none hover:bg-red-1" onClick={()=>onRemove(tag)} size='sm'>
+              <X className="h-3 w-3" />
+            </Button>
+          </Badge>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default MultiText;
